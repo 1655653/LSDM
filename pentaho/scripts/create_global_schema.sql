bbbcb1d49b9954dab5a9505c01e0376aa1fde08d
Alessio Papi <alessio.papi@live.it>
2021-05-11 17:07:22 +0200

    Compatibility : minor changes
    Minor modifications were done to achieve compatibility.
    
    In order to run the main Pentaho integration job, you need:
    	- A user "infint" with password "infint" shall be logged in your
    	PostgreSQL server.
    	- A database "infint" shall be present in your PostgreSQL
    	server, having as owner the "infint" user above.
    
    Every time a dataset/transformation/job/script needs to be referenced
    from Pentaho elements, it has to be via relative path wrt the codebase.
    This is achieved through the Pentaho variables
    ${Internal.Job.Filename.Directory} (in case we are within the context of a job) or
    ${Internal.Transformation.Filename.Directory} (in case of a transformation).
    
    Being both aligned with these practices should be enough to achieve
    compatibility.
    
    Signed-off-by: Alessio Papi <alessio.papi@live.it>

--- pentaho/scripts/create_global_schema.sql
+++ pentaho/scripts/create_global_schema.sql
@@ -1,105 +1,105 @@
-DROP SCHEMA IF EXISTS infint CASCADE;
-CREATE SCHEMA infint;
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
 
 -- =============================================================
 
-CREATE TABLE infint.SoftwareHouse
+CREATE TABLE SoftwareHouse
 (
 	name varchar(300) NOT NULL,
 	website varchar(300),
 	PRIMARY KEY (name)
 );
-CREATE INDEX idx_SoftwareHouse_lookup ON infint.SoftwareHouse(name);
+CREATE INDEX idx_SoftwareHouse_lookup ON SoftwareHouse(name);
 
 -- =============================================================
 
-CREATE TABLE infint.Developer
+CREATE TABLE Developer
 (
 	name varchar(300) NOT NULL,
 	PRIMARY KEY (name),
-	FOREIGN KEY (name) REFERENCES infint.SoftwareHouse(name)
+	FOREIGN KEY (name) REFERENCES SoftwareHouse(name)
 );
-CREATE INDEX idx_Developer_lookup ON infint.Developer(name);
+CREATE INDEX idx_Developer_lookup ON Developer(name);
 
 -- =============================================================
 
-CREATE TABLE infint.Publisher
+CREATE TABLE Publisher
 (
 	name varchar(300) NOT NULL,
 	PRIMARY KEY (name),
-	FOREIGN KEY (name) REFERENCES infint.SoftwareHouse(name)
+	FOREIGN KEY (name) REFERENCES SoftwareHouse(name)
 );
-CREATE INDEX idx_Publisher_lookup ON infint.Publisher(name);
+CREATE INDEX idx_Publisher_lookup ON Publisher(name);
 
 -- =============================================================
 
-CREATE TABLE infint.Videogame
+CREATE TABLE Videogame
 (
 	title VARCHAR(300) NOT NULL,
 	PRIMARY KEY (title)
 );
-CREATE INDEX idx_Videogame_lookup ON infint.Videogame(title);
+CREATE INDEX idx_Videogame_lookup ON Videogame(title);
 
 -- =============================================================
 
-CREATE TABLE infint.City
+CREATE TABLE City
 (
 	name varchar(300) NOT NULL,
 	PRIMARY KEY (name)
 );
-CREATE INDEX idx_City_lookup ON infint.City(name);
+CREATE INDEX idx_City_lookup ON City(name);
 
 -- =============================================================
 
-CREATE TABLE infint.Country
+CREATE TABLE Country
 (
 	name varchar(300) NOT NULL,
 	PRIMARY KEY (name)
 );
-CREATE INDEX idx_Country_lookup ON infint.Country(name);
+CREATE INDEX idx_Country_lookup ON Country(name);
 
 -- =============================================================
 
-CREATE TABLE infint.Publish
+CREATE TABLE Publish
 (
 	videogame VARCHAR(300) NOT NULL,
 	publisher VARCHAR(300) NOT NULL,
 	PRIMARY KEY (videogame, publisher),
-	FOREIGN KEY (videogame) REFERENCES infint.Videogame(title),
-	FOREIGN KEY (publisher) REFERENCES infint.Publisher(name)
+	FOREIGN KEY (videogame) REFERENCES Videogame(title),
+	FOREIGN KEY (publisher) REFERENCES Publisher(name)
 );
 
 -- =============================================================
 
-CREATE TABLE infint.Develop
+CREATE TABLE Develop
 (
 	videogame VARCHAR(300) NOT NULL,
 	developer VARCHAR(300) NOT NULL,
 	PRIMARY KEY (videogame, developer),
-	FOREIGN KEY (videogame) REFERENCES infint.Videogame(title),
-	FOREIGN KEY (developer) REFERENCES infint.Developer(name)
+	FOREIGN KEY (videogame) REFERENCES Videogame(title),
+	FOREIGN KEY (developer) REFERENCES Developer(name)
 );
 
 -- =============================================================
 
-CREATE TABLE infint.LocatedIn
+CREATE TABLE LocatedIn
 (
 	softwareHouse VARCHAR(300),
 	city VARCHAR(300),
 	x_coord NUMERIC(20, 8),
 	y_coord NUMERIC(20, 8),
 	PRIMARY KEY (softwareHouse, city),
-	FOREIGN KEY (softwareHouse) REFERENCES infint.SoftwareHouse(name),
-	FOREIGN KEY (city) REFERENCES infint.City(name)
+	FOREIGN KEY (softwareHouse) REFERENCES SoftwareHouse(name),
+	FOREIGN KEY (city) REFERENCES City(name)
 );
 
 -- =============================================================
 
-CREATE TABLE infint.HasCountry
+CREATE TABLE HasCountry
 (
 	city VARCHAR(300),
 	country VARCHAR(300),
 	PRIMARY KEY (city, country),
-	FOREIGN KEY (city) REFERENCES infint.City(name),
-	FOREIGN KEY (country) REFERENCES infint.Country(name)
+	FOREIGN KEY (city) REFERENCES City(name),
+	FOREIGN KEY (country) REFERENCES Country(name)
 );
